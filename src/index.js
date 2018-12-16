f5100eb26a2117adde46e7380ac27e1fd136b3b7
Aad Versteden <madnificent@gmail.com>
2018-12-16 12:11:39 +0100

    Import from lblod/notulen-importer-service
    This is an initial import from the notulen-importer-service.  It is a
    first try to see if this form of importing operates as expected.
    
    The build setup is based on
    https://www.wouterbulten.nl/blog/tech/publishing-es6-package-to-npm/

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import emberObjectMock from './ember-object-mock';
+import nodeWalker from './node-walker';
+import rdfaContextScanner from './rdfa-context-scanner';
+import richNode from './rich-node';
+
+export default {
+  emberObjectMock,
+  nodeWalker,
+  rdfaContextScanner,
+  richNode
+};
---
+++ src/rich-node.js
@@ -0,0 +1,38 @@
+import {get, set} from './ember-object-mock';
+
+/**
+ * Represents an enriched DOM node.
+ *
+ * The DOM node is available in the 'domNode' property.
+ *
+ * @module editor-core
+ * @class RichNode
+ * @constructor
+ */
+class RichNode {
+  constructor(content) {
+    for( var key in content )
+      this[key] = content[key];
+  }
+  region() {
+    const start = get(this, 'start');
+    const end = get(this, 'end');
+
+    return [ start, end || start ];
+  }
+  length() {
+    const end = get(this, 'end') || 0;
+    const start = get(this, 'start') || 0;
+    const diff = Math.max( 0, end - start );
+    return diff;
+  }
+  isInRegion(start, end) {
+    return get(this, 'start') >= start && get(this, 'end') <= end;
+  }
+  isPartiallyInRegion(start, end) {
+    return ( get(this, 'start') >= start && get(this, 'start') < end )
+      || ( get(this, 'end') > start && get(this, 'end') <= end );
+  }
+}
+
+export default RichNode;
